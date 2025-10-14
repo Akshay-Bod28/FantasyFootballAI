e4ae47dd8a6d04d17d157a1fe098189c89f960dd
akshay bodanapu <akshay@akshays-MacBook-Air.local>
2025-10-14 12:29:29 -0500

    changed to vite server

--- fantasyAI/src/App.jsx
+++ fantasyAI/src/App.jsx
@@ -1,12 +1,11 @@
-import React from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import PredictForm from "./PredictForm";
 
 function App() {
   return (
     <div className="text-center">
-      <h1 className="text-4xl font-bold mb-6 text-green-400">
-        Fantasy Football Predictor
-      </h1>
       <PredictForm />
     </div>
   );
